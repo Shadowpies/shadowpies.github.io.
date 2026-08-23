98028fbaaae0652aeaf2abac328a88ae217f7336
Marcus <marcusgbj@gmail.com>
2026-08-23 20:57:03 +0200

    JS works, shitty but works

--- CV.js
+++ CV.js
@@ -1,3 +1,6 @@
 const theme_button = document.getElementById("theme")
 const page = document.getElementById("page")
 
+theme_button.addEventListener("click", () => {
+    page.classList.toggle("light")
+} );
