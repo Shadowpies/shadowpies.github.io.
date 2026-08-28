ef2e1d0702bb9517a7fb13c11bab829890084c1b
Marcus <marcusgbj@gmail.com>
2026-08-28 13:08:20 +0200

    light and dark mode fixed

--- CV.js
+++ CV.js
@@ -1,6 +1,10 @@
 const theme_button = document.getElementById("theme")
 const page = document.getElementById("page")
+const all_h = document.querySelectorAll("h1, h2, h3, h4, h5, h6")
+const header = document.querySelector(".header_text")
 
 theme_button.addEventListener("click", () => {
     page.classList.toggle("light")
+    header.classList.toggle("light")
+    all_h.forEach((heading) => heading.classList.toggle("light"))
 } );
