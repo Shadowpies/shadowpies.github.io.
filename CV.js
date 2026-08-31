df9fa27a524231ee5c0bf538878d527d89fe1925
Marcus <marcusgbj@gmail.com>
2026-08-31 10:51:50 +0200

    oi

--- CV.js
+++ CV.js
@@ -2,9 +2,11 @@ const theme_button = document.getElementById("theme")
 const page = document.getElementById("page")
 const all_h = document.querySelectorAll("h1, h2, h3, h4, h5, h6")
 const header = document.querySelector(".header_text")
+const left_side = document.querySelector(".left_side")
 
 theme_button.addEventListener("click", () => {
     page.classList.toggle("light")
     header.classList.toggle("light")
     all_h.forEach((heading) => heading.classList.toggle("light"))
+    
 } );
